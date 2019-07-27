817f441de971e1d9a18d68a24fc53fb1bd4246a1
Rilson Almeida <rilson.almeida@gmail.com>
2019-07-27 20:25:40 -0300

    Criando pagina de signup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,10 +34,8 @@ export class MyApp {
 
   initializeApp() {
     this.platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+    this.statusBar.styleDefault();
+    this.splashScreen.hide();
     });
   }
 
