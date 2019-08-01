cb11558e6d09a773c456e099f236ee200cab3d52
Rilson Almeida <rilson.almeida@gmail.com>
2019-07-31 22:11:59 -0300

    Salvando pedido

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -6,6 +6,7 @@ import { CartService } from '../../services/domain/cart.service';
 import { EnderecoDTO } from '../../models/endereco.dto';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
+import { PedidoService } from '../../services/domain/pedido.service';
 
 @IonicPage()
 @Component({
@@ -23,7 +24,8 @@ export class OrderConfirmationPage {
     public navCtrl: NavController, 
     public navParams: NavParams,
     public cartService : CartService,
-    public clienteService : ClienteService) {
+    public clienteService : ClienteService,
+    public pedidoService : PedidoService) {
 
     this.pedido = this.navParams.get('pedido');
   }
@@ -37,7 +39,7 @@ export class OrderConfirmationPage {
         this.endereco = this.findEndereco(this.pedido.enderecoDeEntrega.id, response['enderecos'])
       },
       error => {
-        this.navCtrl.setRoot('HomPage');
+        this.navCtrl.setRoot('HomePage');
       });
   }
 
@@ -49,4 +51,23 @@ export class OrderConfirmationPage {
   total() {
     return this.cartService.total();
   }
+
+  back() {
+    this.navCtrl.setRoot('CartPage');
+  }
+
+  checkout() {
+    this.pedidoService.insert(this.pedido)
+      .subscribe(response => {
+        this.cartService.createOrClearCart();
+        console.log(response.headers.get('location'));
+      },
+      error => {
+        if (error.status == 403) {
+          this.navCtrl.setRoot('HGomePage');
+        }
+      }
+      )
+  }
+
 }
