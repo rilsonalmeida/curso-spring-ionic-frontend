0435d54f76900f9270e5674abeea2b58dba08185
Rilson Almeida <rilson.almeida@gmail.com>
2019-07-31 22:49:25 -0300

    Informando pedido registrado ao usuario

--- src/services/domain/pedido.service.ts
+++ src/services/domain/pedido.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { API_CONFIG } from "../../config/api.config";
+import { PedidoDTO } from "../../models/pedido.dto";
 
 
 
